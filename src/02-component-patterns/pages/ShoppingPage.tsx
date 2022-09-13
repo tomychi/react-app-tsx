9b2f5a81f54fea4945e782e2fbf133dc3df2f4ab
tomychi <tomychi352@gmail.com>
2022-09-13 14:09:00 -0300

    prem-npm

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -21,31 +21,18 @@ export const ShoppingPage = () => {
             <ProductCard 
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues = {{
                     quantity: 4,
-                    maxQuantity: 10,
+                    // maxQuantity: 10,
                 }}
             >
                 {/* Formik me devuelve una funcion y en base a eso trabajo */}
                 {
                     ( {reset, quantity, isMaxQuantityReached, increaseBy, maxQuantity} ) => (
                         <>
-                            <ProductImage className="custom-image" style={{boxShadow: '10px 10px 10px rba(0,0,0,0.2) '  }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-
-                            <button onClick={reset} >Reset</button>
-
-                            <button onClick={()=> increaseBy(-2)} >-2</button>
-
-                            {/* si no se llega al isMaxQuantity, ocultar */}
-
-                            {
-                                (!isMaxQuantityReached && <button onClick={()=> increaseBy(+2)} >+2</button>)
-                            }   
-                            <span>{quantity} - {maxQuantity}</span>
-
+                            <ProductImage />
+                            <ProductTitle  />
+                            <ProductButtons />
                         </>
                     )
                 }
